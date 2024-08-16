7cc1590d5572d7e2f3cea79a69de064104bbdea4
NITA Dan lucian <dlnita@jouve.com>
2024-08-16 13:10:07 +0200

    feat: update migrations

--- storage-service/storage_service/settings.py
+++ storage-service/storage_service/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 ALLOWED_HOSTS = ['localhost' , '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['http://*']
 
 
 # Application definition
